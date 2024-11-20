a94f4662ff6598e14a863b1b70e1a6c6288ec13f
Michael Dearlove <michaeldearlove@yahoo.co.uk>
2024-11-20 13:46:42 +0000

    CSS hide github link

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ logging.getLogger('streamlit').setLevel(logging.ERROR)
 
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+# hide github link on home page
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
 
